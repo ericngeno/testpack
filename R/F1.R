5c66f4277fe43f4b851d404f36ea1917a23a75ba
ericngeno <engeno@ku.edu>
2023-04-19 16:55:07 -0500

    v0.0.1 documentation stuff checked

--- R/F1.R
+++ R/F1.R
@@ -1,3 +1,12 @@
-f1<-function(test="default"){
+#' Title
+#'
+#' @param test (character) the text to paste with "F1". Default="default"
+#'
+#' @return A character vector of length 1
+#' @export
+#'
+#' @examples
+#' f9<-f1 (text = "simple" )
+f1<-function(text="default"){
   return(paste(text,"FI"))
 }
